d9be760d32d16dd065de40e571639b201a8c0466
Vitaly Senokosov <6545415@gmail.com>
2018-10-09 21:27:58 +0300

    .csproj files updated

--- Tests/WebApp.UnitTests/ControllersTests/CategoryControllerTests.cs
+++ Tests/WebApp.UnitTests/ControllersTests/CategoryControllerTests.cs
@@ -17,6 +17,7 @@ namespace WebApp.UnitTests.ControllersTests
             IMockService mockService = new MockService();
             _unitOfWork = mockService.UnitOfWork;
         }
+
         [Fact]
         public void Index_Returns_A_ViewResult_And_List_Of_Categories()
         {
