c15c9622ceae1ae512f9ed0da1101b0e96da5c57
Vitaly Senokosov <6545415@gmail.com>
2018-10-22 18:22:22 +0300

    Adjusting config and tests for MacOS development

--- Tests/WebApp.UnitTests/ControllersTests/CategoryControllerTests.cs
+++ Tests/WebApp.UnitTests/ControllersTests/CategoryControllerTests.cs
@@ -6,12 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 using WebApp.Controllers;
 using WebApp.ViewModels;
 using Xunit;
+using AutoFixture;
+using Moq;
 
 namespace WebApp.UnitTests.ControllersTests
 {
     public class CategoryControllerTests
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly Fixture _fixture;
 
         public CategoryControllerTests()
         {
@@ -38,7 +41,11 @@ namespace WebApp.UnitTests.ControllersTests
         public void Edit_Get_Returns_ViewResult()
         {
             //Arrange
-            var categoryController = new CategoryController(_unitOfWork);
+            var uow = new Mock<IUnitOfWork>();
+            var cat = new Category(); //_fixture.Create<Category>();
+            uow.Setup(x => x.Categories.Get(It.IsAny<int>())).Returns(cat);
+            var categoryController = new CategoryController(uow.Object);
+            var id = _unitOfWork.Categories.GetAll().First().CategoryId;
 
             //Act
             var result = categoryController.Edit(1);
