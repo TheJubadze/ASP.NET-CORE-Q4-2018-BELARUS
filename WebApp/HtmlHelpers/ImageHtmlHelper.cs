3d262b6b49f593a815be9cd0aba4c076157c6e38
Vitaly Senokosov <Vitaliy_Senokosov@epam.com>
2018-11-01 12:15:31 +0300

    Html- and TagHelpers added

---
+++ WebApp/HtmlHelpers/ImageHtmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.HtmlHelpers
+{
+    public static class ImageHtmlHelper
+    {
+        public static HtmlString CreateImageLink(this IHtmlHelper html, string imageId, string content)
+        {
+            var result = $"<a href=\"Images/{imageId}\">{content}</a>";
+            return new HtmlString(result);
+        }
+    }
+}
---
+++ WebApp/TagHelpers/ImageTagHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.TagHelpers
+{
+    public class ImageTagHelper : TagHelper
+    {
+        public string NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var address = "Images/" + NorthwindId;
+            output.Attributes.SetAttribute("href", address);
+            //output.Content.SetContent(address);
+        }
+    }
+}
