7aa6b2b8c72e25268c15c5f5c1e1d0c23c40c3f7
Vitaly Senokosov <Vitaliy_Senokosov@epam.com>
2018-11-13 16:16:58 +0300

    Api controllers added. ConsoleApp added.

---
+++ WebApp/Controllers/CategoryApiController.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/category")]
+    public class CategoryApiController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryApiController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Categories()
+        {
+            var categories = _unitOfWork.Categories.GetAll();
+
+            return Ok(categories);
+        }
+    }
+}
---
+++ WebApp/Controllers/ProductApiController.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/product")]
+    public class ProductApiController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductApiController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Categories()
+        {
+            var categories = _unitOfWork.Products.GetAll();
+
+            return Ok(categories);
+        }
+    }
+}
